21f9d4186bfeb5471972e26c47068c1aeaf3ddae
Christian Hergert <chergert@redhat.com>
2017-06-05 15:51:12 -0700

    theme: add default styling for pillbox

--- src/widgets/dzl-pill-box.c
+++ src/widgets/dzl-pill-box.c
@@ -119,7 +119,7 @@ dzl_pill_box_class_init (DzlPillBoxClass *klass)
                          (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   g_object_class_install_property (object_class, PROP_LABEL, properties [PROP_LABEL]);
 
-  gtk_widget_class_set_css_name (widget_class, "pillbox");
+  gtk_widget_class_set_css_name (widget_class, "dzlpillbox");
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/dazzle/ui/dzl-pill-box.ui");
   gtk_widget_class_bind_template_child (widget_class, DzlPillBox, label);
 }
