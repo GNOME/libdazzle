8b56ae29f74e91cab0e066a780e29b5987383ee7
Christian Hergert <chergert@redhat.com>
2017-06-16 14:35:34 -0700

    example: start on some more examples
    This doesn't fully work yet, but that needs to be fixed and this is a
    reasonable test case to ensure that happens.

--- examples/app/example-window.c
+++ examples/app/example-window.c
@@ -1,4 +1,7 @@
+#include "config.h"
+
 #include <dazzle.h>
+#include <glib/gi18n.h>
 
 #include "example-window.h"
 
@@ -9,6 +12,27 @@ struct _ExampleWindow
 };
 
 G_DEFINE_TYPE (ExampleWindow, example_window, GTK_TYPE_WINDOW)
+
+static const DzlShortcutEntry shortcuts[] = {
+  { "com.example.window.NewDoc", "<control>n", N_("Editing"), N_("Documents"), N_("New Document"), N_("Create a new document") },
+  { "com.example.window.CloseDoc", "<control>w", N_("Editing"), N_("Documents"), N_("Close Document"), N_("Close the current document") },
+};
+
+static void
+new_document_cb (ExampleWindow *self,
+                 gpointer       user_data)
+{
+  g_assert (EXAMPLE_IS_WINDOW (self));
+
+}
+
+static void
+close_document_cb (ExampleWindow *self,
+                   gpointer       user_data)
+{
+  g_assert (EXAMPLE_IS_WINDOW (self));
+
+}
 
 static void
 example_window_class_init (ExampleWindowClass *klass)
@@ -22,7 +46,29 @@ example_window_class_init (ExampleWindowClass *klass)
 static void
 example_window_init (ExampleWindow *self)
 {
+  DzlShortcutController *controller;
+
   gtk_widget_init_template (GTK_WIDGET (self));
+  dzl_shortcut_manager_add_shortcut_entries (NULL, shortcuts, G_N_ELEMENTS (shortcuts), GETTEXT_PACKAGE);
+
+  controller = dzl_shortcut_controller_find (GTK_WIDGET (self));
+
+  dzl_shortcut_manager_set_theme_name (NULL, "default");
+
+  dzl_shortcut_controller_add_command_callback (controller,
+                                                "com.example.window.NewDoc",
+                                                "<control>n",
+                                                (GtkCallback) new_document_cb, NULL, NULL);
+
+  dzl_shortcut_controller_add_command_callback (controller,
+                                                "com.example.window.CloseDoc",
+                                                "<control>w",
+                                                (GtkCallback) close_document_cb, NULL, NULL);
+
+  g_signal_connect_swapped (self,
+                            "key-press-event",
+                            G_CALLBACK (dzl_shortcut_manager_handle_event),
+                            dzl_shortcut_manager_get_default ());
 }
 
 GtkWidget *
