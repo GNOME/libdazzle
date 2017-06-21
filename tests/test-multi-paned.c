2636788898b81e96d0397900de1fc9031d23e95a
Christian Hergert <chergert@redhat.com>
2017-06-21 15:54:09 -0700

    tests: add hook to swap orientation
    We have a small orientation bug and this exposes it in horizontal mode.

--- tests/test-multi-paned.c
+++ tests/test-multi-paned.c
@@ -1,5 +1,15 @@
 #include <dazzle.h>
 
+static void
+swap_orientation (GtkWidget *button,
+                  GtkWidget *paned)
+{
+  GtkOrientation orientation;
+
+  orientation = gtk_orientable_get_orientation (GTK_ORIENTABLE (paned));
+  gtk_orientable_set_orientation (GTK_ORIENTABLE (paned), !orientation);
+}
+
 gint
 main (gint   argc,
       gchar *argv[])
@@ -7,6 +17,8 @@ main (gint   argc,
   GtkBuilder *builder = NULL;
   GtkWindow *window = NULL;
   GError *error = NULL;
+  GObject *button;
+  GObject *paned;
   g_autofree gchar *path = g_build_filename (TEST_DATA_DIR, "test-multi-paned.ui", NULL);
 
   gtk_init (&argc, &argv);
@@ -16,6 +28,10 @@ main (gint   argc,
   builder = gtk_builder_new ();
   gtk_builder_add_from_file (builder, path, &error);
   g_assert_no_error (error);
+
+  paned = gtk_builder_get_object (builder, "paned");
+  button = gtk_builder_get_object (builder, "swap");
+  g_signal_connect (button, "clicked", G_CALLBACK (swap_orientation), paned);
 
   window = GTK_WINDOW (gtk_builder_get_object (builder, "window"));
   g_signal_connect (window, "delete-event", gtk_main_quit, NULL);
