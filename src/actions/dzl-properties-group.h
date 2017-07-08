5b520379bc07430d92b14289241422a0c359afef
Christian Hergert <chergert@redhat.com>
2017-07-08 01:07:53 -0700

    properties-group: use old-style guards
    Everything else is, so let's keep it like that for now.

--- src/actions/dzl-properties-group.h
+++ src/actions/dzl-properties-group.h
@@ -16,7 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef DZL_PROPERTIES_GROUP_H
+#define DZL_PROPERTIES_GROUP_H
 
 #include <gio/gio.h>
 
@@ -46,3 +47,5 @@ void                dzl_properties_group_remove             (DzlPropertiesGroup 
                                                              const gchar        *name);
 
 G_END_DECLS
+
+#endif /* DZL_PROPERTIES_GROUP_H */
