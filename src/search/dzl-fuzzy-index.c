eb82b1bcb4b479ae581a6ee2355ff982ddc5b197
Christian Hergert <chergert@redhat.com>
2018-07-27 20:03:25 -0700

    fuzzy: release metadata variant

--- src/search/dzl-fuzzy-index.c
+++ src/search/dzl-fuzzy-index.c
@@ -110,6 +110,7 @@ dzl_fuzzy_index_finalize (GObject *object)
   g_clear_pointer (&self->keys, g_variant_unref);
   g_clear_pointer (&self->tables, g_variant_dict_unref);
   g_clear_pointer (&self->lookaside, g_variant_unref);
+  g_clear_pointer (&self->metadata, g_variant_dict_unref);
 
   G_OBJECT_CLASS (dzl_fuzzy_index_parent_class)->finalize (object);
 }
