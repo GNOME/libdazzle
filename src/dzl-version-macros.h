57ce6d47e73b706dbdce9fb65ebd50fd3b5da945
Christian Hergert <chergert@redhat.com>
2018-09-12 22:17:41 -0700

    Post-release version bump

--- src/dzl-version-macros.h
+++ src/dzl-version-macros.h
@@ -43,6 +43,7 @@
 
 #define DZL_VERSION_3_28 (G_ENCODE_VERSION (3, 28))
 #define DZL_VERSION_3_30 (G_ENCODE_VERSION (3, 30))
+#define DZL_VERSION_3_32 (G_ENCODE_VERSION (3, 32))
 
 #if (DZL_MINOR_VERSION == 99)
 # define DZL_VERSION_CUR_STABLE (G_ENCODE_VERSION (DZL_MAJOR_VERSION + 1, 0))
@@ -141,6 +142,20 @@
 # define DZL_AVAILABLE_IN_3_30                 DZL_UNAVAILABLE(3, 30)
 #else
 # define DZL_AVAILABLE_IN_3_30                 _DZL_EXTERN
+#endif
+
+#if DZL_VERSION_MIN_REQUIRED >= DZL_VERSION_3_32
+# define DZL_DEPRECATED_IN_3_32                DZL_DEPRECATED
+# define DZL_DEPRECATED_IN_3_32_FOR(f)         DZL_DEPRECATED_FOR(f)
+#else
+# define DZL_DEPRECATED_IN_3_32                _DZL_EXTERN
+# define DZL_DEPRECATED_IN_3_32_FOR(f)         _DZL_EXTERN
+#endif
+
+#if DZL_VERSION_MAX_ALLOWED < DZL_VERSION_3_32
+# define DZL_AVAILABLE_IN_3_32                 DZL_UNAVAILABLE(3, 32)
+#else
+# define DZL_AVAILABLE_IN_3_32                 _DZL_EXTERN
 #endif
 
 #endif /* DZL_VERSION_MACROS_H */
